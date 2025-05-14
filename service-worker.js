796ba469b65e0ed0d29405df3bfc933fe4362474
Momin Sajidali Abidali <mominsajidali.play@gmail.com>
2025-05-14 10:53:35 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const cacheName = "weather-app-cache-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
